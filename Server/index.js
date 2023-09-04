f831a7bd0791d7c46a816efe8d8af9d6f11b448b
diazarm <marceloardiaz@gmail.com>
2023-09-04 00:39:25 -0300

    update

--- Server/index.js
+++ Server/index.js
@@ -4,6 +4,6 @@ const { conn } = require('./src/db.js');
 const PORT = 3001;
 
 server.listen(PORT, ()=> {
-  conn.sync({force:false}); //alter (es como refresh) - force
+  conn.sync({ force : false }); //alter (es como refresh) - force
   console.log(`Listening on port ${PORT}`);
 });
