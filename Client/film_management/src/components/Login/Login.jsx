50f56e32a4bb6491a587788d26a0415151d850ce
diazarm <marceloardiaz@gmail.com>
2023-09-04 23:56:30 -0300

    feat login component

---
+++ Client/film_management/src/components/Login/Login.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import './login.css'; 
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes agregar la lógica para enviar los datos de inicio de sesión al servidor
+    console.log('Datos de inicio de sesión enviados:', formData);
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Iniciar Sesión</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Usuario:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Contraseña:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Iniciar Sesión</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
--- Client/film_management/src/views/Landing/Landing.jsx
+++ Client/film_management/src/views/Landing/Landing.jsx
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom";
 import style from "./Landing.module.css";
+import Login from "../../components/Login/Login";
 
 const Landing = () => {
     return (
         <div className={style.div}>
             <h1 className={style.welcome}>WELCOME!</h1>
+            <Login/>
             <NavLink to="/home">
               <button className={style.home}>Please Push Here!</button>
           </NavLink>
